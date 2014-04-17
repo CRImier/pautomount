afec0504faab89a0fd53f64f7e23097ce77af45d
Arseniy <crimier@yandex.ru>
2014-04-17 09:36:27 +0300

    Added example pautomount.conf file

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mv pautomount.py /usr/bin/pautomount
 chmod +x /usr/bin/pautomount
 mv pautomount.init /etc/init.d/pautomount
 chmod +x /etc/init.d/pautomount
-mv pautomount.conf /etc/pautomount.conf
+mv pautomount.conf.example /etc/pautomount.conf
 update-rc.d pautomount defaults
 echo "Now edit /etc/pautomount.conf so that all the partitions mounted by /etc/fstab are in the exception list."
 echo "Then, set 'noexecute' in the 'globals' section to 'false' or remove this option"
