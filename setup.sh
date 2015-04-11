61e48d0f750ae93cad3434fe2035f18c76b8d42a
CRImier <crimier@yandex.ru>
2015-04-11 00:56:56 +0000

    Added systemctl files

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
-mv pautomount.py /usr/bin/pautomount
+cp pautomount.py /usr/bin/pautomount
+cp pautomount.service /etc/systemd/system/
 chmod +x /usr/bin/pautomount
-mv pautomount.init /etc/init.d/pautomount
+cp pautomount.init /etc/init.d/pautomount
 chmod +x /etc/init.d/pautomount
-mv pautomount.conf.example /etc/pautomount.conf
+cp pautomount.conf.example /etc/pautomount.conf
 update-rc.d pautomount defaults
+systemctl start pautomount
 echo "Now edit /etc/pautomount.conf so that all the partitions mounted by /etc/fstab are in the exception list."
 echo "Then, set 'noexecute' in the 'globals' section to 'false' or remove this option"
